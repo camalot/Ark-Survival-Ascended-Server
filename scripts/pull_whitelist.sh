acd810aa842be483915a2b92fe88ab804114e5b2
Ryan Conrad <camalot@gmail.com>
2023-12-20 15:17:00 -0600

    more logging for whitelist

--- scripts/pull_whitelist.sh
+++ scripts/pull_whitelist.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -n "$ENABLE_WHITELIST" ] || [ "${ENABLE_WHITELIST,,}" != "true" ]; then
+  echo "Whitelist not enabled, exiting: '$ENABLE_WHITELIST'"
   exit 0
 fi
 
 if [ -n "$WHITELIST_URL" ]; then
+  echo "Whitelist URL not set, exiting: '$WHITELIST_URL'"
   exit 0
 fi
 
@@ -12,6 +14,7 @@ ASA_DIR="/usr/games/.wine/drive_c/POK/Steam/steamapps/common/ARK Survival Ascend
 WHITELIST_FILE="$ASA_DIR/Binaries/Win64/PlayersExclusiveJoinList.txt"
 
 if [ ! -f "$WHITELIST_FILE" ]; then
+  echo "Whitelist file not found, exiting: '$WHITELIST_FILE'"
   exit 0
 fi
 
