8a488fdebb5710882cf4da689a1a76d549a0bc41
Ryan Conrad <camalot@gmail.com>
2023-12-22 11:23:29 -0600

    set default to 1000 for PUID/GUID

--- dockerfile
+++ dockerfile
@@ -3,8 +3,9 @@
 FROM scottyhardy/docker-wine:latest
 
 # Add ARG for PUID and PGID with a default value
-ARG PUID="1001"
-ARG PGID="1001"
+# Setting this here, at build time, does not allow a user to change it at runtime
+ARG PUID="1000"
+ARG PGID="1000"
 ARG INI_FILE_VERSION="1.4.6"
 ARG RCON_CLI_VERSION="1.6.4"
 
--- scripts/POK_Update_Monitor.sh
+++ scripts/POK_Update_Monitor.sh
@@ -29,12 +29,12 @@ saved_build_id=$(get_build_id_from_acf)
 current_build_id=$(get_current_build_id)
 
 if [ -z "$saved_build_id" ] || [ "$saved_build_id" != "$current_build_id" ]; then
-  if [ "${DISPLAY_POK_MONITOR_MESSAGE}" = "TRUE" ]; then
+  if [ "${DISPLAY_POK_MONITOR_MESSAGE,,}" = "true" ]; then
     echo "Update available."
   fi
   exit 0
 else
-  if [ "${DISPLAY_POK_MONITOR_MESSAGE}" = "TRUE" ]; then
+  if [ "${DISPLAY_POK_MONITOR_MESSAGE,,}" = "true" ]; then
     echo "No updates."
   fi
   exit 1
